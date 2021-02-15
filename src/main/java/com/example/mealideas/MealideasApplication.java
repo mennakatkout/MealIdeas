3db8b80ca3a7f76d171da5797dd1187fb3457e26
Menna <melshazly.ext@orange.com>
2021-02-15 10:59:51 +0200

    first commit

---
+++ src/main/java/com/example/mealideas/MealideasApplication.java
@@ -0,0 +1,13 @@
+package com.example.mealideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealideasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealideasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mealideas/MealideasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealideasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
