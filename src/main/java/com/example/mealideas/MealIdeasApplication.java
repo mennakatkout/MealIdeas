af9852032858850e87663e1a8cf27645ea5b8cde
Menna <melshazly.ext@orange.com>
2021-02-15 11:23:31 +0200

    modified .gitignore

--- src/main/java/com/example/mealideas/MealIdeasApplication.java
+++ src/main/java/com/example/mealideas/MealIdeasApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MealideasApplication {
-
+public class MealIdeasApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MealideasApplication.class, args);
+		SpringApplication.run(MealIdeasApplication.class, args);
 	}
 
 }
